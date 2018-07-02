58cfb7b4883036ab42681d08bb9c5b86c5dc888f
valin000 <765167664@qq.com>
2018-07-02 10:15:54 +0800

    upload

---
+++ client/app.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+    <Header></Header>
+    <Todo></Todo>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './layout/header.vue'
+import Footer from './layout/footer.jsx'
+import Todo from './views/todo/todo.vue'
+
+export default {
+  components: {
+    Header,Footer,Todo
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
+#loading{
+  position fixed
+  top 0
+  right 0
+  bottom 0
+  left 0
+  background-color rgba(255,255,255,.3)
+  z-index 99
+  display flex
+  align-items center
+  justify-content center
+}
+</style>
