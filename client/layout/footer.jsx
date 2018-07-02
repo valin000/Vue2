68e40ba8e4bd56e6107d2d557c6dfc06c4f721c9
Erik <765167664@qq.com>
2018-07-02 11:11:59 +0800

    upload

--- client/layout/footer.jsx
+++ client/layout/footer.jsx
@@ -1,13 +1,12 @@
 import className from '../assets/styles/footer.styl'
 
-
 export default {
-  data() {
+  data () {
     return {
       author: 'Jokcy'
     }
   },
-  render() {
+  render () {
     return (
       <div id={className.footer}>
         <span>Written by {this.author}</span>
